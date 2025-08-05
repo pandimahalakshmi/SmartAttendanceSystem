5b85b99703e08a27dd8d1e2ebf5ce127014a430a
pandimahalakshmi <pandimahalakshmi5@gmail.com>
2025-08-04 20:33:37 -0700

    Added Smart Attendance System code

--- test_ip_camera.py
+++ test_ip_camera.py
@@ -1,17 +1,15 @@
 import cv2
 
-# Replace with the exact IP shown in your phone's app
-url = 'http://100.84.185.162:8080/video'
-
-cap = cv2.VideoCapture(url)
+url = 'http://100.84.185.162:8080/video'  # Use your IP
+cap = cv2.VideoCapture(url, cv2.CAP_FFMPEG)
 
 while True:
     ret, frame = cap.read()
     if not ret:
+        print("Failed to grab frame")
         break
 
-    cv2.imshow('Mobile Camera - IP Webcam', frame)
-
+    cv2.imshow('IP Webcam Feed', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
